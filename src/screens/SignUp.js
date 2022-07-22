7b4ac99ff0f9a91445c8ac21c427f7ae57964221
Abhishek Kapoor <kapoorabhishek567@gmail.com>
2022-07-22 23:51:58 +0530

    sign up screen

--- src/screens/SignUp.js
+++ src/screens/SignUp.js
@@ -1,15 +1,189 @@
-import React from "react";
+import React,{useState} from "react";
 import {
     StyleSheet,
-    Text
+    View,
+    ScrollView,
+    TouchableOpacity
 } from 'react-native'
 
-const SignUp = ()=>{
-    return(
-        <>
-        <Text>Hello from SignUp</Text>
-        </>
+import storage from '@react-native-firebase/storage'
+import  ProgressBar  from "react-native-progress/Bar";
+
+import ImagePicker 'react-native-image-picker'
+import {options} from '../utils/options'
+
+import{
+    Text,
+    Form,
+    Container,
+    Item,
+    Input,
+    Button,
+    Thumbnail,
+    Content
+} from 'native-base'
+
+//redux 
+import propTypes from 'prop-types'
+import {signUp} from '../action/auth'
+import {connect} from 'react-redux'
+
+const SignUp = ({signUp})=>{
+
+    const [name,setName] = useState('')
+    const [email,setEmail] = useState('')
+    const [password,setPassword] = useState('')
+    const [instaUsername,setInstaUsername] = useState('')
+    const [country,setCountry] = useState('')
+    const [bio,setBio] = useState('')
+    const [image,setImage] = useState(
+     'https://firebase.google.com/downloads/brand-guidelines/PNG/logo-logomark.png'
     )
+
+    const [imageUploading,setImageUploading] = useState(false)
+    set[uploadStatus,setUploadStatus] = useState(null)
+
+    const chooseImage = async()=>{
+        ImagePicker.showImagePicker(options,(response)=>{
+            console.log('Response=',response)
+
+
+
+            if(response.didCancel){
+                console.log('User cancelled image picker')
+            }else if(response.error){
+                console.log('ImagePicker error:',response.error)
+            }else if(response.customButton){
+                console.log('User tapped custom button:', response.customButton)
+            }else{
+                console.log(response)
+                uploadImage(response)
+            }
+
+
+        })
+    }
+
+    const uploadImage = async(response)=>{
+
+        setImageUploading(true)
+        const reference = storage().ref(response.fileName)
+
+        const task = reference.putfile(response.path)
+        task.on('state_changed',(taskSnapshot)=>{
+            const percentage = (taskSnapshot.bytesTransferred/taskSnapshot.totalBytes) *1000            //1000 due to byte
+            setUploadStatus(percentage)
+        })
+
+        task.then(async()=>{
+            const url  = await reference.getDownloadURL()
+            setImage(url)
+            setImageUpload(false)
+        })
+
+    }
+
+    const doSignUp  = async()={
+        signUp({name,emai,password,instaUsername,country,bio,image})
+    }
+
+    return (
+        <Container style={styles.container}>
+          <Content padder>
+            <ScrollView contentContainerStyle={{flexGrow: 1}}>
+              <View style={styles.imageContainer}>
+                <TouchableOpacity onPress={chooseImage}>
+                  <Thumbnail large source={{uri: image}} />
+                </TouchableOpacity>
+              </View>
+    
+              {imageUploading && (
+                <ProgressBar progress={uploadStatus} style={styles.progress} />
+              )}
+    
+              <Form>
+                <Item regular style={styles.formItem}>
+                  <Input
+                    placeholder="name"
+                    value={name}
+                    style={{color: '#eee'}}
+                    onChangeText={(text) => setName(text)}
+                  />
+                </Item>
+                <Item regular style={styles.formItem}>
+                  <Input
+                    placeholder="email"
+                    value={email}
+                    style={{color: '#eee'}}
+                    onChangeText={(text) => setEmail(text)}
+                  />
+                </Item>
+                <Item regular style={styles.formItem}>
+                  <Input
+                    placeholder="password"
+                    value={password}
+                    secureTextEntry={true}
+                    style={{color: '#eee'}}
+                    onChangeText={(text) => setPassword(text)}
+                  />
+                </Item>
+                <Item regular style={styles.formItem}>
+                  <Input
+                    placeholder="Instagram user name"
+                    value={instaUserName}
+                    style={{color: '#eee'}}
+                    onChangeText={(text) => setInstaUserName(text)}
+                  />
+                </Item>
+                <Item regular style={styles.formItem}>
+                  <Input
+                    placeholder="Your Short Bio"
+                    value={bio}
+                    style={{color: '#eee'}}
+                    onChangeText={(text) => setBio(text)}
+                  />
+                </Item>
+                <Item regular style={styles.formItem}>
+                  <Input
+                    placeholder="country"
+                    value={country}
+                    style={{color: '#eee'}}
+                    onChangeText={(text) => setCountry(text)}
+                  />
+                </Item>
+                <Button regular block onPress={doSignUp}>
+                  <Text>SignUp</Text>
+                </Button>
+              </Form>
+            </ScrollView>
+          </Content>
+        </Container>
+      );
+    
 }
 
-export default SignUp
+const  mapDispatchToProps = {
+    signUp: (data)=> signUp(data)
+}
+
+SignUp.propTypes ={
+    signUp: propTypes.func.isRequired
+}
+
+export default connect(null,mapDispatchToProps)(SignUp)
+
+const styles = StyleSheet.create({
+    container: {
+      backgroundColor: '#1b262c',
+      flex: 1,
+      justifyContent: 'flex-start',
+    },
+    imageContainer: {
+      alignItems: 'center',
+      marginVertical: 5,
+    },
+    progress: {width: null, marginBottom: 20},
+    formItem: {
+      marginBottom: 20,
+    },
+  });
